7394924f0a61901f44e3f5fad787a762ba8e97b8
Yvonne <mulys001@gmail.com>
2020-10-03 13:20:03 +0800

    update index

--- ym-script.js
+++ ym-script.js
@@ -1,3 +1,12 @@
 var apiKey = "1da6b12f081b6014f18ea9f7c1ea494e31deb443";
 var searchTerm = "";
 
+function holiday () {
+  var queryURL = "https://calendarific.com/api/v2/holidays?api_key=" + apiKey + "&country=AU&year=2020"
+
+  $.ajax({
+    url: queryURL,
+    method: "GET"
+  }).then(function (response) {
+    console.log(response)
+})}
