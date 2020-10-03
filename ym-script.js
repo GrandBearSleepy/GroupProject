d9b43392e212c6a9301c5beee324ca99607b7710
Yvonne <mulys001@gmail.com>
2020-10-03 13:30:44 +0800

    Initial ajax call

--- ym-script.js
+++ ym-script.js
@@ -1,8 +1,10 @@
 var apiKey = "1da6b12f081b6014f18ea9f7c1ea494e31deb443";
 var searchTerm = "";
+var holidayType = "National"
+
 
 function holiday () {
-  var queryURL = "https://calendarific.com/api/v2/holidays?api_key=" + apiKey + "&country=AU&year=2020"
+  var queryURL = "https://calendarific.com/api/v2/holidays?api_key=" + apiKey + "&country=AU&year=2019&type=" + holidayType
 
   $.ajax({
     url: queryURL,
@@ -10,3 +12,5 @@ function holiday () {
   }).then(function (response) {
     console.log(response)
 })}
+
+holiday()
